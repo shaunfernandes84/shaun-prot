7457b392c3dc6965356e42d32804af399dac86c7
Shaun Fernandes <145692712+shaunfernandes84@users.noreply.github.com>
2024-02-08 07:34:59 -0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,11 @@ import { NavLink } from "react-router-dom";
 import { logo } from "../assets/images";
 
 const Navbar = () => {
+  
   return (
     <header className='header'>
       <NavLink to='/'>
-        <img src="ss.png" height="50px" width = "200px" alt='logo' className='w-18 h-18 object-contain' />
+        <img src="https://logos.textgiraffe.com/logos/logo-name/Shaun-designstyle-summer-m.png" height="50px" width = "100px" alt='logo' className='w-18 h-18 object-contain' />
       </NavLink>
       <nav className='flex text-lg gap-7 font-medium'>
         <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
